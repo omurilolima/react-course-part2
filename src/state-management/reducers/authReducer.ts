b4bd226fd201caeb9f96f5d104c7f3a754541622
omurilolima <msl@cesar.school>
2025-06-27 12:16:16 +0100

    Exercise - Implementing a LoginStatus component with Reducers

---
+++ src/state-management/reducers/authReducer.ts
@@ -0,0 +1,18 @@
+interface LoginAction {
+	type: "LOGIN";
+	username: string;
+}
+
+interface LogoutAction {
+	type: "LOGOUT";
+}
+
+type AuthAction = LoginAction | LogoutAction;
+
+const authReducer = (user: string, action: AuthAction): string => {
+	if (action.type === "LOGIN") return action.username;
+	if (action.type === "LOGOUT") return "";
+	return user;
+};
+
+export default authReducer;
