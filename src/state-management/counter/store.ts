24cb8c45db5e97908a0ab81dfc1bbf8bdb62c5e7
omurilolima <msl@cesar.school>
2025-07-09 18:21:59 +0100

    Inspecting Stores with Zustand DevTools

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -4,7 +4,13 @@
 // npm i zustand@4.3.7
 // use an interface to define the shape of the store
 
+// For inspecting stores with Zustand DevTools, install:
+// npm i simple-zustand-devtools@1.1.0
+// then, in the store.ts import the function mountStoreDevtool
+// install the type declaration for node: npm i -D @types/node
+
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore {
 	counter: number;
@@ -22,5 +28,12 @@ const useCounterStore = create<CounterStore>((set) => ({
 	increment: () => set((store) => ({ counter: store.counter + 1 })),
 	reset: () => set(() => ({ max: 10 })),
 }));
+
+// If in development environment, call the mountStoreDevtool
+// passagin a name to the store and the custom hook
+// Back to the browser, you will see the name of the store in the components tab
+// Click it and you will see the content of the store
+if (process.env.NODE_ENV === "development")
+	mountStoreDevtool("Counter Store", useCounterStore);
 
 export default useCounterStore;
