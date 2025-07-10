72457258d106138f68c432e10ea18df78124b3cd
omurilolima <msl@cesar.school>
2025-07-10 20:57:42 +0100

    Creating a Navigation Bar with Nested Routes

--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -1,12 +1,15 @@
-import NavBar from './NavBar';
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const Layout = () => {
-  return (
-    <>
-      <NavBar />
-      <div id="main"></div>
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<div id="main">
+				<Outlet />
+			</div>
+		</>
+	);
 };
 
 export default Layout;
--- src/routing/NavBar.tsx
+++ src/routing/NavBar.tsx
@@ -1,30 +1,32 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-  return (
-    <nav
-      className="navbar navbar-expand-lg"
-      style={{ background: '#f0f0f0', marginBottom: '1rem' }}
-    >
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          My App
-        </a>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Users
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav
+			className="navbar navbar-expand-lg"
+			style={{ background: "#f0f0f0", marginBottom: "1rem" }}
+		>
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					My App
+				</a>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link to="/" className="nav-link">
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/users" className="nav-link">
+								Users
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default NavBar;
