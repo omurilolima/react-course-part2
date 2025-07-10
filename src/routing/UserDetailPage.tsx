6868a14f61f039389acf36b5f028fb59e9f2babf
omurilolima <msl@cesar.school>
2025-07-10 17:56:41 +0100

    Getting Data about the Current Route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,18 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+	// Extract parameter values from the URL
+	const params = useParams();
+
+	// Access and update query string parameters
+	// NOTE: you should call this function only inside event handlers or inside an effect
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	// Access the current location
+	const location = useLocation();
+	console.log(location);
+
+	return <p>User</p>;
 };
 
 export default UserDetailPage;
