9f4023b1c88974d3804e09427eba7e43cb9fbfdc
omurilolima <msl@cesar.school>
2025-07-10 16:44:56 +0100

    Navigation with React Router

--- src/routing/ContactPage.tsx
+++ src/routing/ContactPage.tsx
@@ -1,14 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const ContactPage = () => {
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        // Redirect the user to the home page
-      }}
-    >
-      <button className="btn btn-primary">Submit</button>
-    </form>
-  );
+	const navigate = useNavigate();
+	return (
+		<form
+			onSubmit={(event) => {
+				event.preventDefault();
+				navigate("/");
+			}}
+		>
+			<button className="btn btn-primary">Submit</button>
+		</form>
+	);
 };
 
 export default ContactPage;
