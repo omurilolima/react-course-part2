22350100338a969010862b6819ce4ddc402e2ba6
omurilolima <msl@cesar.school>
2025-07-11 12:53:07 +0100

    Group routes for enforcing layout and business rules in a single place

--- src/routing/UsersPage.tsx
+++ src/routing/UsersPage.tsx
@@ -1,11 +1,7 @@
-import useAuth from "./hooks/useAuth";
+import { Outlet } from "react-router-dom";
 import UserList from "./UserList";
-import { Navigate, Outlet } from "react-router-dom";
 
 const UsersPage = () => {
-	const { user } = useAuth();
-	if (!user) return <Navigate to={"/login"} />;
-
 	return (
 		<div className="row">
 			<div className="col">
