b347bf951ab335671fdd23e64058b66890a3e71b
omurilolima <msl@cesar.school>
2025-07-11 12:17:01 +0100

    Handling Erros in the ErrorPage

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,10 +1,15 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
-  return (
-    <>
-      <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </>
-  );
+	const error = useRouteError();
+	console.log(error);
+
+	return (
+		<>
+			<h1>Oops...</h1>
+			<p>{isRouteErrorResponse(error) ? "Invalid page" : "Unexpected error"}</p>
+		</>
+	);
 };
 
 export default ErrorPage;
